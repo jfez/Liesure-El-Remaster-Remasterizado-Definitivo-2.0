5da5227a01bf876cb127341f5ca210929842a207
Jorge Fernández <44475543+ixa10@users.noreply.github.com>
2019-01-27 22:19:24 +0100

    anim alfredo

---
+++ Juego Plataformas 2D/Assets/Scripts/animAlfredo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animAlfredo : MonoBehaviour {
+
+    public bool eating;
+    public int scene;
+
+    private Animator anim;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        if (scene == 0)         //Halls --> idle
+        {
+            eating = false;
+        }
+
+        else if (scene == 1)    //Level2 --> comiendo
+        {
+            eating = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("Eating", eating);
+    }
+}
